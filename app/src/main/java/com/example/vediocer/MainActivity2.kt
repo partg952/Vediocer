2a1eb4633141573215d8fc81b1a677e3eafa2ee2
parth <dankparth@gmail.com>
2021-02-04 09:18:52 +0530

    first commit

--- app/src/main/java/com/example/vediocer/MainActivity2.kt
+++ app/src/main/java/com/example/vediocer/MainActivity2.kt
@@ -3,6 +3,8 @@ package com.example.vediocer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.FirebaseAuth
@@ -22,6 +24,23 @@ class MainActivity2 : AppCompatActivity() {
         view.adapter = adapter(supportFragmentManager)
         tab.setupWithViewPager(view)
 
+    }
+    
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.sign->{
+                FirebaseAuth.getInstance().signOut()
+                var intent = Intent(this,MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
